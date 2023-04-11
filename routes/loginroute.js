f078c7eecdec8f7cdcf0e3a5996ac64f91f1c3fa
Orumpati <orumpatisaikrishna00.com>
2023-04-11 15:09:17 +0530

    hj

--- routes/loginroute.js
+++ routes/loginroute.js
@@ -44,7 +44,10 @@ router.post('/loginDetails',async(req,res,next)=>{
                          TrukCapacity:doc.TrukCapacity,
                          TrukNumber:doc.TrukNumber,
                          TrukType:doc.TrukType,
-                         referalCode:doc.referalCode
+                         referalCode:doc.referalCode,
+                         referTo:doc.refferedTo,
+                         signupReferalCode:doc.signupReferalCode
+
                      
                         
                     })
